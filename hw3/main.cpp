648a0dbeabfae8863cb906c9b5b7c8a1104cdc99
chenhanlee <ya060505@gmail.com>
2020-04-28 09:30:40 +0800

    add m

--- hw3/main.cpp
+++ hw3/main.cpp
@@ -49,25 +49,87 @@ I2C i2c( PTD9,PTD8);
 
 Serial pc(USBTX, USBRX);
 
+DigitalOut led1(LED1);
+//DigitalOut led2(LED2);
+//DigitalOut led3(LED3);
+
+InterruptIn sw2(SW2);
+
+EventQueue queue;
+
 int m_addr = FXOS8700CQ_SLAVE_ADDR1;
+
+float t[100][4];
 
 
 void FXOS8700CQ_readRegs(int addr, uint8_t * data, int len);
 
 void FXOS8700CQ_writeRegs(uint8_t * data, int len);
 
+void logger(int m, int n);
+
+/*void test_irq() {
+
+   //queue.call(logger, 1, 100);
+   printf("test \r\n");
+   //wait(1);
+   //led1 = !led1;///////////////
+
+}*/
+
+void sw2_irq() {
+
+   queue.call(logger, 1, 100);
+   //printf("inq \r\n");
+   //led1 = 1;//////////////////
+   //led2=0;
+   //led3=1;//////////////
+   //queue.call(test_irq);
+   //wait(1);
+   //led1 = !led1;///////////////
+
+}
 
 int main() {
 
-
    pc.baud(115200);
 
+   //EventQueue queue;
+
+   led1 = 1;
+   //wait(1);///////////////
+   //led1 = !led1;
+   //wait(1);
+   //led1 = !led1;///////////////
+
+   queue.call(&logger, 0, 1);
+   //printf("beforeq \r\n");
+   //queue.call(printf, "called immediately\r\n");
+   //queue.call(&sw2_irq);
+   //printf("afterq \r\n");
+
+   sw2.rise(&sw2_irq);
+
+   queue.dispatch();
+
+   //while(1);
+
+}
+
+void logger(int m, int n) {
+
+   //pc.baud(115200);
+   //led1 = 1;//////////////////
+   //led2 = 1;
+   //led3 = 0;///////////////////
+   //wait(5);
+   //led1 = 1;///////////////
 
    uint8_t who_am_i, data[2], res[6];
 
    int16_t acc16;
 
-   float t[3];
+   //float t[3];
 
 
    // Enable the FXOS8700Q
@@ -87,9 +149,9 @@ int main() {
    FXOS8700CQ_readRegs(FXOS8700Q_WHOAMI, &who_am_i, 1);
 
 
-   pc.printf("Here is %x\r\n", who_am_i);
+   //pc.printf("Here is %x\r\n", who_am_i);
 
-   while (true) {
+   for (int i=m; i<n; i++) {
 
 
       FXOS8700CQ_readRegs(FXOS8700Q_OUT_X_MSB, res, 6);
@@ -101,7 +163,7 @@ int main() {
 
          acc16 -= UINT14_MAX;
 
-      t[0] = ((float)acc16) / 4096.0f;
+      t[i][0] = ((float)acc16) / 4096.0f;
 
 
       acc16 = (res[2] << 6) | (res[3] >> 2);
@@ -110,7 +172,7 @@ int main() {
 
          acc16 -= UINT14_MAX;
 
-      t[1] = ((float)acc16) / 4096.0f;
+      t[i][1] = ((float)acc16) / 4096.0f;
 
 
       acc16 = (res[4] << 6) | (res[5] >> 2);
@@ -119,32 +181,50 @@ int main() {
 
          acc16 -= UINT14_MAX;
 
-      t[2] = ((float)acc16) / 4096.0f;
+      t[i][2] = ((float)acc16) / 4096.0f;
+
+      if(t[i][2]<0 || t[i][2]*t[i][2]<0.5)
+         t[i][3] = 1;
+      else
+         t[i][3] = 0;
+      
 
 
-      printf("FXOS8700Q ACC: X=%1.4f(%x%x) Y=%1.4f(%x%x) Z=%1.4f(%x%x)\r\n",\
+      //printf("FXOS8700Q ACC: X=%1.4f(%x%x) Y=%1.4f(%x%x) Z=%1.4f(%x%x)\r\n",\
 
-            t[0], res[0], res[1],\
+         //   t[0], res[0], res[1],\
 
-            t[1], res[2], res[3],\
+           
 
-            t[2], res[4], res[5]\
+      led1 = 0;
 
-      );
+      wait(0.05);
 
+      led1 = 1;
 
-      wait(1.0);
+      wait(0.05);
 
    }
 
+   if(n==100) {
+      for (int i = 0; i < 100; i++){
+         for (int j = 0; j < 4; j++){
+
+            pc.printf("%1.3f\r\n", t[i][j]);
+
+            wait(0.1);
+
+         }
+      }
+   }
 }
 
 
 void FXOS8700CQ_readRegs(int addr, uint8_t * data, int len) {
 
-   char t = addr;
+   char c = addr;
 
-   i2c.write(m_addr, &t, 1, true);
+   i2c.write(m_addr, &c, 1, true);
 
    i2c.read(m_addr, (char *)data, len);
 
